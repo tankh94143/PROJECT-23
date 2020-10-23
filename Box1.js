12134c1d927479c36a2f33daf2d87c1f6be07cd8
tankh94143 <71911101+tankh94143@users.noreply.github.com>
2020-10-23 21:07:39 +0800

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,28 @@
+class {
+    constructor(x,y,w,h){
+    this.body = Bodies.rectangle(x,y,w,h)
+    World.add (world,this.body)
+    this.w = w
+    this.h = h
+    
+    
+    }
+    
+    display(){
+    push()
+    
+    translate(this.body.position.x, this.body.position.y)
+    rotate(this.body.angle)
+    
+    rectMode(CENTER)
+    rect(0,0,this.w, this.h)
+    pop()
+    }
+    }
+    
+    
+    
+    
+    
+    
+    
